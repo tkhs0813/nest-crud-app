7cbc717cf90f8c6fd11321e98bc879d8b614bbf9
empitsu <oneok0813@gmail.com>
2020-05-04 22:21:02 +0900

    add contacts controller

--- server/src/contacts/contacts.module.ts
+++ server/src/contacts/contacts.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ContactsService } from './contacts/contacts.service';
+import { ContactsController } from './contacts/contacts.controller';
 
 @Module({
-  providers: [ContactsService]
+  providers: [ContactsService],
+  controllers: [ContactsController]
 })
 export class ContactsModule {}
